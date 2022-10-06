15b2277d58ec61d5729061faa07877977e455cb4
KAVIN6302 <113257249+KAVIN6302@users.noreply.github.com>
2022-10-06 22:24:13 +0530

    submit main file

---
+++ Assignments/Team Member 1 - NITHISH B/Assignment 02/app.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+@app.route("/home")
+def homepage():
+    return render_template('home.html')
+
+@app.route("/about")
+def aboutpage():
+    return render_template('about.html')
+
+@app.route("/signin")
+def signinpage():
+    return render_template('signin.html')
+
+@app.route("/signup")
+def signuppage():
+    return render_template('signup.html')
